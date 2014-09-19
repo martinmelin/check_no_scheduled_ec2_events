fac739c20a8a003aaf429781b0affadbbe9f6631
Martin Melin <martin@tictail.com>
2014-09-18 20:19:36 -0400

    Add docopt for -h message

--- check_no_scheduled_ec2_events/__init__.py
+++ check_no_scheduled_ec2_events/__init__.py
@@ -1,5 +1,19 @@
 # -*- encoding: utf-8 -*-
+"""A Nagios-style script that will check if the Amazon EC2 instance it is running
+on has any events scheduled, like a system reboot.
+
+Exit code 2 if any events are scheduled, 0 otherwise. Useful in combination
+with Nagios and/or NRPE.
+
+Usage:
+    check_no_scheduled_ec2_events
+    check_no_scheduled_ec2_events -h | --help
+
+No options are given. The script assumes it is running in an EC2 instance, and
+will grab the current instance id from the instance metadata and AWS
+credentials from the current environment."""
 import sys
+import docopt
 import boto.ec2
 import boto.utils
 
@@ -8,6 +22,9 @@ def get_region_from_az(az):
     return az[:-1]
 
 def check():
+    # No arguments yet, but let docopt handle --help:
+    docopt.docopt(__doc__)
+
     # Grab current instance metadata (will hang if not in EC2)
     current_instance = boto.utils.get_instance_metadata()
     instance_id = current_instance['instance-id']
@@ -24,3 +41,6 @@ def check():
         sys.exit(2)
 
     print "OK: instance %s has no scheduled events" % instance_id
+
+if __name__ == "__main__":
+    check()
